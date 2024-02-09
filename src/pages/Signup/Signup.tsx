b51d6e398f8097e4ad87d5028b705b64d8ab34f3
Katherina <fedoseeva2292@gmail.com>
2024-02-09 13:00:04 +0100

    show password, eye symbol

--- src/pages/Signup/Signup.tsx
+++ src/pages/Signup/Signup.tsx
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Signup.scss';
 
+interface Toggle {
+  password: boolean;
+  confPassword: boolean;
+}
+
 function Signup() {
+  const [toggle, setToggle] = useState<Toggle>({
+    password: false,
+    confPassword: false,
+  });
+
+  const handleToggle = (field: keyof Toggle) => {
+    setToggle((prev) => ({
+      ...prev,
+      [field]: !prev[field],
+    }));
+  };
+
   return (
     <div className="signup_container">
       <div className="signup_wrapper">
@@ -16,13 +33,20 @@ function Signup() {
           <div className="signup_group">
             <label htmlFor="email">
               Электронная почта
-              <input type="email" name="email" id="email" />
+              <input type="email" name="email" id="email" required />
             </label>
           </div>
           <div className="signup_group">
             <label htmlFor="password">
               Пароль
-              <input type="password" name="password" id="password" />
+              <input type="password" name="password" id="password" required />
+              <i
+                onClick={() => handleToggle('password')}
+                className={`fa fa-eye${
+                  toggle.password ? '-slash' : ''
+                } password_toggle`}
+                aria-hidden="true"
+              ></i>
             </label>
           </div>
           <div className="signup_group">
@@ -32,7 +56,15 @@ function Signup() {
                 type="password"
                 name="confirmPassword"
                 id="confirmPassword"
+                required
               />
+              <i
+                onClick={() => handleToggle('confPassword')}
+                className={`fa fa-eye${
+                  toggle.confPassword ? '-slash' : ''
+                } password_toggle`}
+                aria-hidden="true"
+              ></i>
             </label>
           </div>
           <button type="submit">Зарегистрироваться</button>
